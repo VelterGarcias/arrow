69d4af801ec1384b0211b532e491541813b9f56c
VelterGarcias <64234043+VelterGarcias@users.noreply.github.com>
2020-07-29 03:21:21 -0300

    Create arrow.js

---
+++ arrow.js
@@ -0,0 +1,30 @@
+var audio = document.createElement('audio');
+audio.src = 'https://www.botecodigital.info/exemplos/audio/i_am_the_doctor.mp3';
+audio.play();
+audio.loop = true;
+audio.muted =true;
+
+function play(){audio.currentTime = 0;audio.muted = false;}
+function stop(){audio.muted = true;}
+
+
+
+
+function addClassNameListener(elemId) {
+    var elem = document.querySelector(elemId);
+    var lastClassName = elem.className;
+    window.setInterval( function() {   
+       var className = elem.className;
+        if (className !== lastClassName) {
+            
+            if (elem.classList.contains("enabled")){
+play();  	 
+             	setTimeout(function(){if (confirm("NOVA TAREFA, BORA TRABALHAR!!")) {stop();} else {stop();};},500); 
+
+            }
+            lastClassName = className;
+        }
+    },10);
+}
+
+addClassNameListener(".start-button")
